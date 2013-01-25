f4cee40abb6d5894afd8b643a3f5fb18e6970993
Alexey <sunrizz@outlook.com>
2013-01-25 15:03:35 +0900

    main commit

---
+++ GTD-G/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+
+namespace GTD_G
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public List<string> ListP = new List<string>();
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ApplicationBarMenuItem_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/list.xaml", UriKind.Relative));
+        }
+
+        public object NameLP { get; set; }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("Name") == true)
+            {
+                FirstListBox.Items.Add(IsolatedStorageSettings.ApplicationSettings["Name"].ToString());
+            }
+            else
+            {
+            }
+
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
---
+++ GTD-G/list.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+using System.Windows;
+
+namespace GTD_G
+{
+    public partial class list : PhoneApplicationPage
+    {
+        int[] Cents = {-2, -1, 0, 1, 2};
+
+   public list()
+        {
+            InitializeComponent();           
+            Summ.ItemsSource = Cents;
+        }
+
+        public void ApplicationBarMenuItem_Click(object sender, EventArgs e)
+        {
+            String nulltext = NameToDo.Text.ToString();
+            if (string.IsNullOrEmpty(NameToDo.Text))
+            {
+                MessageBox.Show("Нужно заполнить название!");
+            }
+            else
+            {
+
+                String NameTo = NameToDo.Text.ToString();
+                IsolatedStorageSettings.ApplicationSettings["Name"] = NameTo;
+
+                int Sum = (int)Summ.SelectedItem;
+                IsolatedStorageSettings.ApplicationSettings["Value"] = Sum;
+                IsolatedStorageSettings.ApplicationSettings.Save();
+                NavigationService.GoBack();
+            }
+        }
+        public object stringint { get; set; }
+    }
+}
